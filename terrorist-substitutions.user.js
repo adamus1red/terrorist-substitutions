13acb0aad9668ee99ce2f04b630725cf317e15a2
Greg Ebdon <ebdong@gmail.com>
2017-06-04 16:01:58 +0100

    Create terrorist-substitutions.user.js
    First version. With thanks to Chris Addison for the concept, and xkcd/mcef/istepaniuk for the base user script.

---
+++ terrorist-substitutions.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        terrorist-substitutions
+// @namespace   twitter.com/Vanderdecken
+// @description Substitutions that make reading about terrorists more accurate
+// @include     http://www.bbc.co.uk/*
+// @include     http://www.bbc.com/*
+// @include     http://www.cbc.ca/*
+// @include     http://www.theguardian.com/*
+// @include     http://www.telegraph.co.uk/*
+// @include     http://www.theonion.com/*
+// @include     http://www.foxnews.com/*
+// @include     https://www.reddit.com/*
+// @include     https://www.washingtonpost.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+(function() {
+    var substitutions,
+        textNodes,
+        regexps = {};
+
+    substitutions = {
+        "terrorists": "these silly cunts",
+        "the terrorist": "the silly cunt",
+        "counter-terrorist": "counter-cuntery",
+        "counter-terrorism": "anti-cuntbucket",
+        "terrorism": "cuntery",
+        "terrorist": "silly cunt"
+    }
+
+    for (var key in substitutions) {
+        regexps[key] = new RegExp(key, 'gi');
+    }
+
+    textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (var i = 0; i < textNodes.snapshotLength; i++) {
+        var node = textNodes.snapshotItem(i);
+        node.data = substituteTextIn(node.data);
+    }
+
+    function substituteTextIn(text) {
+        for (var key in substitutions) {
+            text = text.replace(regexps[key], substitutions[key]);
+        }
+        return text;
+    }
+
+})();
